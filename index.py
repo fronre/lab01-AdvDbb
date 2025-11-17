742e2fc4948037f54a41b2c8a019c3c6ec3cbb89
fronre <islamhala35@gmail.com>
2025-11-17 19:20:44 +0100

    Add function to read a page from heap file with validation

--- index.py
+++ index.py
@@ -6,4 +6,19 @@ PAGE_SIZE = 4096
 def create_heap_file(file_name):
     with open(file_name, 'wb'):
         pass
-    
+def read_page(file_name, page_number):
+        size = os.path.getsize(file_name)
+        last = size // PAGE_SIZE - 1
+
+        if page_number < 0 or page_number > last:
+            raise ValueError("Invalid page number")
+
+        with open(file_name, 'rb') as f:
+            f.seek(page_number * PAGE_SIZE)
+            data = f.read(PAGE_SIZE)
+
+        if len(data) != PAGE_SIZE:
+            raise IOError("Incomplete page read")
+
+        return data
+
