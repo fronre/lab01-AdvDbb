d432a8594842ed985dae2cef116c7dec0900f206
fronre <islamhala35@gmail.com>
2025-11-17 19:24:24 +0100

    add testing read pages

--- test_heap/test.py
+++ test_heap/test.py
@@ -1,12 +1,42 @@
-from lab01-AdvDbb.index import create_heap_file
 import os
 
-def test_create_heap_file():
-    name = "tdd_heap.dat"
-    if os.path.exists(name):
-        os.remove(name)
+PAGE_SIZE = 4096
 
-    create_heap_file(name)
+def create_test_file(file_name, num_pages=3):
+    with open(file_name, 'wb') as f:
+        for i in range(num_pages):
+            f.write(bytes([i % 256] * PAGE_SIZE))
 
-    assert os.path.exists(name)
-    assert os.path.getsize(name) == 0
+
+def read_page(file_name, param):
+    pass
+
+
+def test_read_page():
+    file_name = "test_heap.dat"
+    create_test_file(file_name, num_pages=3)
+
+    data = read_page(file_name, 0)
+    assert len(data) == PAGE_SIZE
+    assert data[0] == 0
+
+    data = read_page(file_name, 2)
+    assert len(data) == PAGE_SIZE
+    assert data[0] == 2
+
+    try:
+        read_page(file_name, 5)
+        assert False, "Expected ValueError"
+    except ValueError:
+        pass
+
+    try:
+        read_page(file_name, -1)
+        assert False, "Expected ValueError"
+    except ValueError:
+        pass
+
+    print("All tests passed!")
+    os.remove(file_name)
+
+test_read_page()
